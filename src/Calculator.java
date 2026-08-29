7cd15003befae687cec013f277e5e1383bad804a
JohnI5-0 <kingofspeed554@gmail.com>
2026-08-28 22:42:52 -0400

    Finished up the program

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,24 +1,8 @@
 public class Calculator{
     private int decimal;
     private String binary;
-    private int bit;
-    public Calculator(){
+    public Calculator(String binary, int decimal){
         this.decimal = 0;
         this.binary ="";
-        this.bit = 0;
-    }
-    public static int[] bitArrBuilder(int bit){
-        int[] twoComp = new int[bit - 1];
-        int i = 0;
-        int comp = 1;
-        for(i = 0; i < bit -1; i++){
-            twoComp[i] = comp;
-            comp = comp * 2;
-        }
-        return twoComp;
-    }
-    public String toBinary(int[] array){
-
-
     }
 }
--- src/ComputingConverter.java
+++ src/ComputingConverter.java
@@ -3,9 +3,12 @@ public class ComputingConverter {
     public static void main(String[] args){
         Scanner stdinScanner = new Scanner (System.in);
         String userInput = "";
+        String binaryOutput;
         int userInputNum = 0;
         int mainCounter = 0;
         int bitCounter = 0;
+        int numOutput = 0;
+        double userInputDouble;
         System.out.println("Welcome to Computing Converter");
         while (mainCounter < 1){
             System.out.println("Select an option");
@@ -13,29 +16,38 @@ public class ComputingConverter {
             System.out.println("2) Binary to Decimal");
             System.out.println("3) Quit");
             userInput = stdinScanner.nextLine();
-            if(userInput.equals("1"){
+            if(userInput.equals("1")){
                 bitCounter = 0;
+                System.out.println("Please enter a whole decimal number");
                 while (bitCounter < 1){
-                    System.out.println("4 bit, 6 bit, or 8 bit?");
-                    System.out.println("4");
-                    System.out.println("6");
-                    System.out.println("8");
                     try{
                         userInputNum = Integer.parseInt(stdinScanner.nextLine());
+                        userInputDouble = userInputNum;
                         bitCounter++;
                     }catch (NumberFormatException e){
-                        System.out.println("Please enter a valid option");
-                    }
-                    if (userInputNum == 4){
-                        int[] array = Calculator.bitArrBuilder(userInputNum);
-                    }else if (userInputNum == 6){
-                        int[] array = Calculator.bitArrBuilder(userInputNum);
-                    }else if (userInputNum == 8){
-                        int[] array = Calculator.bitArrBuilder(userInputNum);
+                        System.out.println("Invalid Input, please enter a whole decimal number");
                     }
                 }
+                binaryOutput = toBinary(userInputNum);
+                new Calculator(binaryOutput, userInputNum);
+                System.out.println(userInputNum + " to binary is " + binaryOutput);
             }else if(userInput.equals("2")){
-
+                bitCounter = 0;
+                System.out.println("Please enter a binary number");
+                while (bitCounter < 1){
+                    try{
+                        userInputNum = Integer.parseInt(stdinScanner.nextLine());
+                        userInputDouble = userInputNum;
+                        bitCounter++;
+                    }catch (NumberFormatException e){
+                        System.out.println("Invalid Input, please enter a whole decimal number");
+                    }
+                }
+                userInput = String.valueOf(userInputNum);
+                int[] binaryLength = new int[userInput.length()];
+                numOutput = toDecimal(binaryLength, userInput);
+                new Calculator(userInput, numOutput);
+                System.out.println(userInputNum + " to decimal is " + numOutput);
             }else if(userInput.equals("3")){
                 System.out.println("Goodbye");
                 mainCounter++;
@@ -43,5 +55,45 @@ public class ComputingConverter {
                 System.out.println("Sorry, " + userInput + " is not a valid option");
             }
         }
+    }
+    public static String toBinary(double num){
+        String outputBin = "";
+        double decimal = num;
+        int arrCounter = 0;
+        while (num > 0){
+            if(num % 2 == 0){
+                num = num / 2;
+                arrCounter = arrCounter + 1;
+            }else{
+                num = num / 2;
+                num = num - .5;
+                arrCounter = arrCounter + 1;
+            }
+        }
+        int[] bitCounter = new int[arrCounter];
+        for(int i = bitCounter.length - 1; i >= 0; i--){
+            if(decimal % 2 == 0){
+                decimal = decimal / 2;
+                bitCounter[i] = 0;
+            }else{
+                decimal = decimal / 2;
+                decimal = decimal - .5;
+                bitCounter[i] = 1;
+            }
+        }
+        for(int i = 0; i < bitCounter.length; i++){
+            outputBin = outputBin + bitCounter[i];
+        }
+        return outputBin;
+    }
+    public static int toDecimal(int[] binaryArr, String binInput){
+        int outputDec = 0;
+        for(int i = 0; i < binInput.length(); i++){
+            binaryArr[i] = binInput.charAt(i) - '0';
+        }
+        for(int i = 0; i < binaryArr.length; i++){
+            outputDec = 2 * outputDec + binaryArr[i];
+        }
+        return outputDec;
     }
 }
